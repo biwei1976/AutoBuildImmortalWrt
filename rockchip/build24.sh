ffca53e2e712202d59b3c8d26e14e27c3d6e4524
biwei1976 <biwei1976@gmail.com>
2026-03-04 22:30:48 +0800

    Update build24.sh

--- rockchip/build24.sh
+++ rockchip/build24.sh
@@ -64,6 +64,13 @@ PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+PACKAGES="$PACKAGES luci-app-ddns"
+PACKAGES="$PACKAGES ddns-scripts"
+PACKAGES="$PACKAGES ddns-scripts-cloudflare"
+PACKAGES="$PACKAGES wget-ssl"
+PACKAGES="$PACKAGES ca-bundle"
+PACKAGES="$PACKAGES ca-certificates"
+PACKAGES="$PACKAGES libustream-openssl"
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
